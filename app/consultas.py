ff9fe29752665593009b634e582c5c0f02dfcd1f
Daniel Niebles <pasovert@gmail.com>
2022-09-30 21:42:28 -0500

    agregando archivo faltantes

--- app/consultas.py
+++ app/consultas.py
@@ -1,6 +1,7 @@
 from app import *
 from app.config import *
 
+## Consulta Estudiante
 def consultarEstudiantes():
     try:
         cur = mysql.connection.cursor()
@@ -18,11 +19,11 @@ def consultarEstudiantes():
                     'nombres': resultado[4],
                     'apellidos': resultado[5],
                     'fecha_nacimiento': resultado[6],
-                    'estado': resultado[10]
+                    'estado': resultado[9]
                 }
             )
 
-            return jsonify(data)
+        return jsonify(data)
 
     except Exception as e:
         print(e)
@@ -46,25 +47,196 @@ def consultarEstudiante(codigoEstudiante):
                     'nombres': resultado[4],
                     'apellidos': resultado[5],
                     'fecha_nacimiento': resultado[6],
-                    'estado': resultado[10]
+                    'estado': resultado[9]
                 }
             )
 
-            return jsonify(data)
+        return jsonify(data)
 
     except Exception as e:
         print(e)
     finally:
         cur.close()
 
+
+## Consulta Usuario
+
+def consultarUsuarios():
+    try:
+        cur = mysql.connection.cursor()
+        sql = "call obtenerUsuarios()"
+        cur.execute(sql)
+        res = cur.fetchall()
+        data = []
+        for resultado in res:
+            print(resultado)
+            aux = {
+                    'id': resultado[0],
+                    'tipo_usuario': resultado[1],
+                    'codigo': resultado[2],
+                    'username': resultado[3],
+                    'password': resultado[4],
+                    'fecha_creacion': resultado[5],
+                    'estado': resultado[6],
+                    'nota': resultado[7]
+                }
+            data.append(aux)
+
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+
 def consultarUsuario(codigoUsuario):
-    pass
+    try:
+        cur = mysql.connection.cursor()
+        sql = "call obtenerUsuario('{0}')"
+        cur.execute(sql.format(codigoUsuario))
+        res = cur.fetchall()
+        data = []
+        for resultado in res:
+            data.append(
+                {
+                    'id': resultado[0],
+                    'tipo_usuario': resultado[1],
+                    'codigo': resultado[2],
+                    'username': resultado[3],
+                    'password': resultado[4],
+                    'fecha_creacion': resultado[5],
+                    'estado': resultado[6],
+                    'nota': resultado[7]
+                }
+            )
+
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+
+## Consulta Materia
+
 
 def consultarMateria(codigoMateria):
-    pass
+    try:
+        cur = mysql.connection.cursor()
+        sql = "call obtenerMateria('{0}')"
+        cur.execute(sql.format(codigoMateria))
+        res = cur.fetchall()
+        data = []
+        for resultado in res:
+            data.append(
+                {
+                    'id': resultado[0],
+                    'codigo': resultado[1],
+                    'nombre': resultado[2],
+                    'estado': resultado[3]
+                }
+            )
+
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+def consultarMaterias():
+    try:
+        cur = mysql.connection.cursor()
+        sql = "call obtenerMaterias()"
+        cur.execute(sql)
+        res = cur.fetchall()
+        data = []
+        for resultado in res:
+            data.append(
+                {
+                    'id': resultado[0],
+                    'codigo': resultado[1],
+                    'nombre': resultado[2],
+                    'estado': resultado[3]
+                }
+            )
+
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+
+## Consulta nota
+
 
 def consultarNotasEstudiante(codigoEstudiante):
-    pass
+    try:
+        cur = mysql.connection.cursor()
+        sql = "call obtenerNotasEstudiante('{0}')"
+        cur.execute(sql.format(codigoEstudiante))
+        res = cur.fetchall()
+        data = []
+        for resultado in res:
+            data.append(
+                {
+                    'id': resultado[0],
+                    'codigo': resultado[1],
+                    'periodo': resultado[2],
+                    'tipo': resultado[10],
+                    'nombre_profesor': resultado[3],
+                    'asignatura': resultado[4],
+                    'curso': resultado[5],
+                    'nombre_Estudiante': resultado[6],
+                    'calificacion': resultado[7],
+                    'anotacion': resultado[8],
+                    'estado': resultado[9]
+                }
+            )
+
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
+
+## Consulta Profesor
+
+
+def consultarProfesores():
+    try:
+        cur = mysql.connection.cursor()
+        sql = "call obtenerProfesores()"
+        cur.execute(sql)
+        res = cur.fetchall()
+        data = []
+        for resultado in res:
+            data.append(
+                {
+                    'id': resultado[0],
+                    'tipo_id': resultado[1],
+                    'codigo': resultado[2],
+                    'nacionalidad': resultado[3],
+                    'nombres': resultado[4],
+                    'apellidos': resultado[5],
+                    'fecha_nacimiento': resultado[6],
+                    'estado': resultado[10]
+                }
+            )
+
+        return jsonify(data)
+
+    except Exception as e:
+        print(e)
+    finally:
+        cur.close()
+
 
 def consultarProfesor(codigoProfesor):
     try:
@@ -87,7 +259,7 @@ def consultarProfesor(codigoProfesor):
                 }
             )
 
-            return jsonify(data)
+        return jsonify(data)
 
     except Exception as e:
         print(e)
--- app/routesObtener.py
+++ app/routesObtener.py
@@ -6,17 +6,41 @@ from app import consultas
 def inicio():
     return "<h1>Hola Mundo</h1>"
 
+
+## Usuario
+
+
 @cross_origin
 @app.route('/obtenerUsuario/<codigoUsuario>', methods=['GET'])
 def obtenerUsuario(codigoUsuario):
-    mensaje = f"<h1>El codigo es: {codigoUsuario} </h1>"
+    mensaje = consultas.consultarUsuario(codigoUsuario)
     return mensaje
+
+@cross_origin
+@app.route('/obtenerUsuarios', methods=['GET'])
+def obtenerUsuarios():
+    mensaje = consultas.consultarUsuarios()
+    return mensaje
+
+
+
+## Estudiante
+
 
 @cross_origin
 @app.route('/obtenerEstudiante/<codigoEstudiante>', methods=['GET'])
 def obtenerEstudiante(codigoEstudiante):
-    mensaje = f"<h1>El codigo es: {codigoEstudiante} </h1>"
+    mensaje = consultas.consultarEstudiante(codigoEstudiante)
     return mensaje
+
+@cross_origin
+@app.route('/obtenerEstudiantes', methods=['GET'])
+def obtenerEstudiantes():
+    mensaje = consultas.consultarEstudiantes()
+    return mensaje
+
+
+## Profesor
 
 @cross_origin
 @app.route('/obtenerProfesor/<codigoProfesor>', methods=['GET'])
@@ -25,15 +49,29 @@ def obtenerProfesor(codigoProfesor):
     return mensaje
 
 @cross_origin
+@app.route('/obtenerProfesores', methods=['GET'])
+def obtenerProfesores():
+    mensaje = consultas.consultarProfesores()
+    return mensaje
+
+## Materias / Asignaturas
+
+@cross_origin
 @app.route('/obtenerMateria/<codigoMateria>', methods=['GET'])
 def obtenerMateria(codigoMateria):
-    mensaje = f"<h1>El codigo es: {codigoMateria} </h1>"
+    mensaje = consultas.consultarMateria(codigoMateria)
+    return mensaje
+
+@cross_origin
+@app.route('/obtenerMaterias', methods=['GET'])
+def obtenerMaterias():
+    mensaje = consultas.consultarMaterias()
     return mensaje
 
 @cross_origin
 @app.route('/obtenerNotasEstudiante/<codigoEstudiante>', methods=['GET'])
 def obtenerNotasEstudiante(codigoEstudiante):
-    mensaje = f"<h1>El codigo es: {codigoEstudiante} </h1>"
+    mensaje = consultas.consultarNotasEstudiante(codigoEstudiante)
     return mensaje
 
 
